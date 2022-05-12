81bb37b1f12d8ed99309d51e262255a9cc876c32
Marcos Antonio Villegas Padilla <marcvill26@gmail.com>
2022-05-12 22:54:58 +0200

    new repo

---
+++ comic-store/src/pages/DcComics/DcComics.jsx
@@ -0,0 +1,38 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import DcComic from '../DcCards/DcCards';
+
+
+const DcComics = () => {
+    const [dccomics, setDcComics]= useState([]);
+    useEffect(() => {
+        axios
+        .get("dcComic.json")
+        .then(response => setDcComics(response.data))
+    },[]);
+
+    return (
+        <div className="dc-list">
+           {dccomics.map((comicsDc) => {
+            return(
+                <DcComic
+                    key={comicsDc.id}
+                    id={comicsDc.id}
+                    hero={comicsDc.hero}
+                    nameHero={comicsDc.nameHero}
+                    writer={comicsDc.writer}
+                    year={comicsDc.year}
+                    image={comicsDc.image}
+                    description={comicsDc.description}
+                    collections={comicsDc.collections}
+                    penciler={comicsDc.penciler}
+                    issues={comicsDc.issues}
+                />
+            )
+           })}
+        </div>
+    );
+}
+
+export default DcComics;
+
