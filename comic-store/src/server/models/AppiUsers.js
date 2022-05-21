f90723c7607b8fe4ae57d68d2ec323fce60a08e3
Estefania Valdez <stefy_vali95@hotmail.com>
2022-05-21 12:50:54 +0200

    update

--- comic-store/src/server/models/AppiUsers.js
+++ comic-store/src/server/models/AppiUsers.js
@@ -5,7 +5,8 @@ const Schema = mongoose.Schema;
 const appiRegisterSchema = new Schema ({
     name: {type: String, required:true},
     lastName: {type: String},
-    age: {type: Number, required: true},
+    age: { type: Number, required: true },
+    rol:{type:String, required:true},
     Comics:[{type: mongoose.Types.ObjectId, ref:'Comic'}],
 
 },{
