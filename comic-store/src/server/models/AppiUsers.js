617cb7a072e3885b69a6b9e77383c07e96df38aa
Oscar Canedo <oscar.canedo@bootcamp-upgrade.com>
2022-05-21 12:15:49 +0200

    userRouter

--- comic-store/src/server/models/AppiUsers.js
+++ comic-store/src/server/models/AppiUsers.js
@@ -6,12 +6,13 @@ const appiRegisterSchema = new Schema ({
     name: {type: String, required:true},
     lastName: {type: String},
     age: {type: Number, required: true},
+    rol: { type: String, required: true},
     Comics:[{type: mongoose.Types.ObjectId, ref:'Comic'}],
 
 },{
     timestamps:true
 });
 
-const RegisterUsers = mongoose.model('Cliente', appiRegisterSchema);
+const User = mongoose.model('Cliente', appiRegisterSchema);
 
-module.exports = RegisterUsers;
+module.exports = User;
